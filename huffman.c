d3fb9ab43eeb89368fa01dd5c743b8a11629cba5
Teodor Stefu <Teodorstefu@gmail.com>
2017-12-02 18:05:31 +0200

    Fix stack problem

--- huffman.c
+++ huffman.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "huffman.h" 
+#include "huffman.h"
 #include "frequency.h"
 #include "priority_queue.h"
 
@@ -16,7 +16,7 @@ node_t* init_node(char data) {
 
 
 int decode_bytes_for_chunk(node_t *root, char *buffer, unsigned long long int nbits, node_t **remainig_node, char* out_buffer) {
-    
+
     int i, j;
     node_t *node = *remainig_node;
     unsigned long long int nbytes = nbits % 8 == 0 ? nbits/8 : nbits/8 + 1;
@@ -26,10 +26,10 @@ int decode_bytes_for_chunk(node_t *root, char *buffer, unsigned long long int nb
 
     for (i = 0; i < nbytes; i++) {
         for (j = 7 ; j >= 0 && count_bits < nbits; j--) {
-            
+
             /* get bit */
             bit = (buffer[i] >> j) & 1;
-            
+
             if (bit == 0) {
                 /* go left in tree*/
                 node = node->left;
@@ -54,7 +54,7 @@ int decode_bytes_for_chunk(node_t *root, char *buffer, unsigned long long int nb
 
 
 int decode_bytes_for_chunk_pthreads(node_t *root, char *buffer, unsigned long long int nbits, char* out_buffer) {
-    
+
     int i, j;
     node_t *node = root;
     unsigned long long int nbytes = nbits % 8 == 0 ? nbits/8 : nbits/8 + 1;
@@ -63,10 +63,10 @@ int decode_bytes_for_chunk_pthreads(node_t *root, char *buffer, unsigned long lo
     int length = 0;
     for (i = 0; i < nbytes; i++) {
         for (j = 7 ; j >= 0 && count_bits < nbits; j--) {
-            
+
             /* get bit */
             bit = (buffer[i] >> j) & 1;
-            
+
             if (bit == 0) {
                 /* go left in tree*/
                 node = node->left;
@@ -93,7 +93,7 @@ int decode_bytes_for_chunk_pthreads(node_t *root, char *buffer, unsigned long lo
 void decode_bytes(FILE *in_fp, FILE *out_fp, node_t *root, unsigned long long int nbits) {
     size_t nread = 0;
     unsigned long long int total_bytes = 0L;
-    unsigned long long int nbytes = nbits % 8L == 0L ? nbits / 8L : nbits/8L + 1L; 
+    unsigned long long int nbytes = nbits % 8L == 0L ? nbits / 8L : nbits/8L + 1L;
     char *aux_buf = (char *) calloc (CHUNK, sizeof(char));
     char *result = (char *) calloc (nbytes, sizeof(char));
     node_t *remainig_node;
@@ -101,15 +101,17 @@ void decode_bytes(FILE *in_fp, FILE *out_fp, node_t *root, unsigned long long in
 
     int length = 0;
     int minimum_size = CHUNK * 8;
+    // fprintf(stderr, "in decode");
 
     while((nread = fread(aux_buf, 1, CHUNK, in_fp)) > 0) {
+        fprintf(stderr, "in decode");
         if (nbits > minimum_size) {
             length = decode_bytes_for_chunk(root, aux_buf, (CHUNK * 8), &remainig_node, result);
             nbits -= minimum_size;
         } else {
             length = decode_bytes_for_chunk(root, aux_buf, nbits, &remainig_node, result);
         }
-        
+
         /* write in out file the result for that CHUNK*/
         fwrite(result, 1, length, out_fp);
 
@@ -130,7 +132,8 @@ void decode_bytes(FILE *in_fp, FILE *out_fp, node_t *root, unsigned long long in
 unsigned long long int write_codification_for_input_file(char **codification, FILE* input_fp, FILE* output_fp) {
     size_t nread = 0;
     int chunk_size = 0;
-    char buf[CHUNK], output_buffer[CHUNK];
+    char *buf = calloc(CHUNK, sizeof(char));
+    char *output_buffer = calloc(CHUNK, sizeof(char));
     char output_char = 0;
     int output_buffer_contor = 0;
     int contor = 7;
@@ -153,7 +156,7 @@ unsigned long long int write_codification_for_input_file(char **codification, FI
     return bits;
 }
 
-void write_codification_for_chunk(char *chunk, int chunk_size, char **codification, char* output_buffer, 
+void write_codification_for_chunk(char *chunk, int chunk_size, char **codification, char* output_buffer,
     int *output_buffer_contor, char* output_char, int* contor, unsigned long long int* bits) {
     int i, j;
 
@@ -173,7 +176,7 @@ void write_codification_for_chunk(char *chunk, int chunk_size, char **codificati
     }
 }
 
-void write_codification_for_chunk_pthreads(char *chunk, int index, int upper_limit, char **codification, 
+void write_codification_for_chunk_pthreads(char *chunk, int index, int upper_limit, char **codification,
     char *output_buffer, int *output_buffer_contor, unsigned long long int *bits) {
 
     *output_buffer_contor = index;
@@ -198,7 +201,7 @@ void write_codification_for_chunk_pthreads(char *chunk, int index, int upper_lim
                 output_char = 0;
                 contor = 7;
             }
-        }   
+        }
     }
 
     if (contor > -1 && contor < 7) {
@@ -252,7 +255,7 @@ void write_codification(FILE* codification_fp, char **codification) {
     for (i = 0; i < MAX_BITS_CODE; i ++) {
         if (codification[i] != NULL) {
             sprintf(output_string, "%c : %s\n", i, codification[i]);
-            fwrite(output_string, strlen(codification[i]) + 5, 1, codification_fp);            
+            fwrite(output_string, strlen(codification[i]) + 5, 1, codification_fp);
             memset(output_string, 0, MAX_BITS_CODE + 5);
         }
     }
@@ -268,7 +271,7 @@ char** read_configuration(FILE *codification_fp, unsigned long long int *nbits) 
     int i;
     size_t line_length;
     int codification_length;
-        
+
     // read number of lines from codification
     fscanf(codification_fp, "%d", &codification_length);
     fgetc(codification_fp);
@@ -281,15 +284,15 @@ char** read_configuration(FILE *codification_fp, unsigned long long int *nbits) 
             nread = getline(&line, &line_length, codification_fp);
             if (nread != -1) {
                 line[nread-1] = '\0';
-                codification[(unsigned char)index] = strdup(line+3);    
+                codification[(unsigned char)index] = strdup(line+3);
             }
         } else {
             line[nread-1] = '\0';
             sscanf(line, "%c", &index);
-            codification[(unsigned char)index] = strdup(line+4);    
+            codification[(unsigned char)index] = strdup(line+4);
         }
-    } 
-    
+    }
+
 
     // read number of bits
     fscanf(codification_fp, "%llu", nbits);
@@ -306,7 +309,7 @@ char **read_configuration_pthreads(FILE *codification_fp, unsigned long long int
     int i;
     size_t line_length;
     int codification_length;
-        
+
     // read number of lines from codification
     fscanf(codification_fp, "%d", &codification_length);
     fgetc(codification_fp);
@@ -319,15 +322,15 @@ char **read_configuration_pthreads(FILE *codification_fp, unsigned long long int
             nread = getline(&line, &line_length, codification_fp);
             if (nread != -1) {
                 line[nread-1] = '\0';
-                codification[(unsigned char)index] = strdup(line+3);    
+                codification[(unsigned char)index] = strdup(line+3);
             }
         } else {
             line[nread-1] = '\0';
             sscanf(line, "%c", &index);
-            codification[(unsigned char)index] = strdup(line+4);    
+            codification[(unsigned char)index] = strdup(line+4);
         }
-    } 
-    
+    }
+
     int threads_no = 0;
     // read number of threads and bits
     fscanf(codification_fp, "%d\n%lu\n", &threads_no, size);
@@ -346,12 +349,12 @@ void find_codification(node_t *node, char *path, int level, char **codification)
         path[level] = 48;
         find_codification(node->left, path, level + 1, codification);
     }
-    
+
     if (node->right != NULL){
         path[level] = 49;
         find_codification(node->right, path, level + 1, codification);
     }
- 
+
     /* if a leaf is found */
     if (!(node->left) && !(node->right)){
         path[level] = '\0';
@@ -367,12 +370,12 @@ void print_codes(node_t *root, char *path, int level) {
         path[level] = 48;
         print_codes(root->left, path, level + 1);
     }
-    
+
     if (root->right != NULL){
         path[level] = 49;
         print_codes(root->right, path, level + 1);
     }
- 
+
     /* if a leaf is found */
     if (!(root->left) && !(root->right)){
         path[level] = '\0';
@@ -390,7 +393,7 @@ node_t* build_huffman_tree_from_codification(char **codification) {
     // init the root node
     node_t* root = init_node('#');
     node_t* node = root;
-    
+
     for (i = 0; i < 128; i ++) {
         if (codification[i]) {
             node = root;
@@ -435,7 +438,7 @@ node_t* build_huffman_tree(unsigned long long int* frequecy){
         root->data = '#';
         root->left = left;
         root->right = right;
-        
+
         /* insert root node into the heap */
         push(h, root);
     }
